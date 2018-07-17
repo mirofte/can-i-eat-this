03e51c58ff65ffc37f4dab9f8368ba44f5a8915a
Marian IROFTE <mirofte@pentalog.com>
2018-07-17 15:47:42 +0300

    Add the grid/table list view

--- src/Components/FoodsList.js
+++ src/Components/FoodsList.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
 import * as actions from '../Actions/Foods.actions';
 import { connect } from 'react-redux';
-import { Row, Col, Alert  } from 'antd';
-import Food from './Food'
+import '../Css/FoodsList.css';
+import { Row, Col, Alert, Table, Radio, Icon } from 'antd';
+import Food from './Food';
 
 class FoodsList extends Component{
+	
+	constructor(props){
+		super(props);
+		this.state = {
+			listView : 'grid',
+		}
+	}
 	
 	componentDidMount = () => {
 		this.props._getFoods('');
 	};
+	
+	changeListView = (e) => {
+		const listView = e.target.value;
+		this.setState({ listView: listView });
+	};
 		
 	render = () => {
 		const { foods } = this.props;
+		const { listView } = this.state;
+		
+		let foodsList = '';
+		
 		if(foods.length === 0){
 			return <Alert
 				message="No food Food"
@@ -19,14 +36,40 @@ class FoodsList extends Component{
 				type="error"
 			/>
 		}
-		return (
-			
-			<div style={{padding:20}}>
-				<Row gutter={16}>
+		
+		if(listView === 'table'){
+			const columns = [{
+				title: 'Name',
+				dataIndex: 'name',
+				key: 'name',
+			  }, {
+				title: 'Type',
+				dataIndex: 'foodType.name',
+				key: 'foodType.name',
+			  }];
+		  
+			const expandedRowRender = food => <img alt={food.name} style={{ width : 200, height : 200}} src={food.image} />;
+			foodsList = <Table rowKey="id" expandedRowRender={expandedRowRender} columns={columns} dataSource={foods} />;
+		}
+		if(listView === 'grid'){
+			foodsList = <Row gutter={16}>
 					{foods.map( food => {
 						return <Col xs={24} lg={6} key={food.id} style={{ paddingBottom: 20}}><Food food={food}/></Col>
 					})}
-				</Row>
+				</Row>;
+		}
+		return (
+			
+			<div style={{padding:20}}>
+				<div className="change-list-view">
+					<Radio.Group defaultValue="grid" onChange={this.changeListView}>
+						<Radio.Button key="grid" value="grid">Grid <Icon type="credit-card" /></Radio.Button>
+						<Radio.Button key="table" value="table">Table <Icon type="table" /></Radio.Button>
+					</Radio.Group>
+				</div>
+				<div >
+				{ foodsList }
+				</div>
 			</div>
 		);
 	}
